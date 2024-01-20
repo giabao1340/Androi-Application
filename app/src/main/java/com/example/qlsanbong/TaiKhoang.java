24ebd475dbf9cbf739ada69a0be7a500cd094d46
BanhBao1340 <banhbaolun0609042gmail.com>
2024-01-20 17:58:16 +0700

    them san cua toi va tai khoang

---
+++ app/src/main/java/com/example/qlsanbong/TaiKhoang.java
@@ -0,0 +1,14 @@
+package com.example.qlsanbong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaiKhoang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tai_khoang);
+    }
+}
