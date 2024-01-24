58e59cb5c91cb76cdbb117869d8ed95d16c1dc87
Gia bảo <luonggiabao060904@gmail.com>
2024-01-24 21:46:52 +0700

    them admin home

---
+++ app/src/main/java/Admin/AdminHome.java
@@ -0,0 +1,16 @@
+package Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.qlsanbong.R;
+
+public class AdminHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+    }
+}
